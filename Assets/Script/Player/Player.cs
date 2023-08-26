2ddc92ca6d274e8cf86dc5ca64c98fdacad410e3
BrennoTreggi <134560249+BrennoTreggi@users.noreply.github.com>
2023-08-25 23:50:16 -0300

    frection player
    frection player

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myrygibody;
-    public Vector2 velocitty;
-    public float speed;
+    public Vector2 friction = new Vector2(.1f, 0);
+    public float Speed;
+    public float ForceJump = 1.5f;
     private void Update()
+    {
+        MyPlayer();
+        PlayerJump();
+    }
+
+    private void MyPlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myrygibody.MovePosition(myrygibody.position - velocitty * Time.deltaTime);
-            myrygibody.velocity = new Vector2(-speed, myrygibody.velocity.y); 
+            myrygibody.velocity = new Vector2(-Speed, myrygibody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myrygibody.MovePosition(myrygibody.position + velocitty * Time.deltaTime);
-            myrygibody.velocity = new Vector2(+speed, myrygibody.velocity.y);
+            myrygibody.velocity = new Vector2(+Speed, myrygibody.velocity.y);
 
+        }
+        if (myrygibody.velocity.x > 0)
+        {
+            myrygibody.velocity -= friction; 
+        }
+        else if (myrygibody.velocity.x < 0)
+        {
+            myrygibody.velocity += friction;
+
+        }
+
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myrygibody.velocity = Vector2.up * ForceJump;
         }
     }
 }
