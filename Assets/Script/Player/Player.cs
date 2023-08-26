95ded6021cb32288addf23dc61e06841e26bbd79
BrennoTreggi <134560249+BrennoTreggi@users.noreply.github.com>
2023-08-26 00:23:08 -0300

    Speed Run
    Add Speed Caracter - Speed Rum

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,9 @@ public class Player : MonoBehaviour
     public Vector2 friction = new Vector2(.1f, 0);
     public float Speed;
     public float ForceJump = 1.5f;
+    public float Speedcaracter;
+   
+    private float Speedforce;
     private void Update()
     {
         MyPlayer();
@@ -16,15 +20,22 @@ public class Player : MonoBehaviour
 
     private void MyPlayer()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftControl))
+        
+            Speedforce = Speedcaracter;
+        else
+                Speedforce = Speed;
+        
+
+       if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myrygibody.MovePosition(myrygibody.position - velocitty * Time.deltaTime);
-            myrygibody.velocity = new Vector2(-Speed, myrygibody.velocity.y);
+            myrygibody.velocity = new Vector2(-Speedforce, myrygibody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myrygibody.MovePosition(myrygibody.position + velocitty * Time.deltaTime);
-            myrygibody.velocity = new Vector2(+Speed, myrygibody.velocity.y);
+            myrygibody.velocity = new Vector2(+Speedforce, myrygibody.velocity.y);
 
         }
         if (myrygibody.velocity.x > 0)
