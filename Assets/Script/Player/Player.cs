aaf5fb90de5f72d901a29c9f973ddc7da3f86b7f
BrennoTreggi <134560249+BrennoTreggi@users.noreply.github.com>
2023-09-21 01:58:08 -0300

    Script Enimy

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -2,16 +2,25 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Player : MonoBehaviour
 {
+    [Header("SpeedeSetup")]
     public Rigidbody2D myrygibody;
     public Vector2 friction = new Vector2(.1f, 0);
     public float Speed;
     public float ForceJump = 1.5f;
+
+
+    [Header("AniomationSetup")]
+    public float ScaleCollider = 0.7f;
+    public float JumpScaleY = 1.5f;
+    public float JumpScaleX = 0.7f;
+    public float jumpduration = .3f;
+    public Ease ease = Ease.OutBack;
     public float Speedcaracter;
-   
     private float Speedforce;
+
     private void Update()
     {
         MyPlayer();
@@ -20,7 +29,7 @@ public class Player : MonoBehaviour
 
     private void MyPlayer()
     {
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftShift))
         
             Speedforce = Speedcaracter;
         else
@@ -51,10 +60,21 @@ public class Player : MonoBehaviour
     }
 
     private void PlayerJump()
-    {
+    { 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myrygibody.velocity = Vector2.up * ForceJump;
+            myrygibody.transform.localScale = Vector2.one;
+            DOTween.Kill(myrygibody.transform);
+            DOTween.Kill(myrygibody.transform);
+            ScaleJump();
         }
+
+    }
+    private void ScaleJump()
+    {
+        myrygibody.transform.DOScaleX(JumpScaleX, ScaleCollider).SetLoops(1, LoopType.Restart).SetEase(ease);
+        myrygibody.transform.DOScaleY(JumpScaleY, jumpduration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myrygibody.transform.DOScaleX(JumpScaleX, jumpduration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
